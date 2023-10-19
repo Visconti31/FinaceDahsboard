8ac4a8daec1d7d646f42c208cfe49656d33fb3bd
Joao Pedro Portella Guedes Visconti <JOVSI@vestas.com>
2023-10-19 14:31:55 +0100

    Start to create the folder and file structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,29 @@
-import { createTheme } from "@mui/material/styles"
-import { CssBaseline, ThemeProvider } from "@mui/material"
-import { useMemo } from "react"
-import { themeSettings } from "./theme"
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { Box } from '@mui/material'
+import { createTheme } from '@mui/material/styles'
+import { useMemo } from 'react'
+import { themeSettings } from './theme'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Navbar from '@/scenes/navbar'
 
 function App() {
-
-  const theme = useMemo(()=> createTheme(themeSettings), [])
+  const theme = useMemo(() => createTheme(themeSettings), [])
 
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<div>Dashboard</div>} />
+              <Route path="/predictions" element={<div>Predictions</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   )
 }
